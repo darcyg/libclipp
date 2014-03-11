cbb326a0ad4cdc67b991f012df2aebfd12a768a7
Diego Lago <diego.lago.gonzalez@gmail.com>
2014-03-11 09:50:46 +0100

    More tests.

--- test/update-var.cpp
+++ test/update-var.cpp
@@ -11,15 +11,28 @@ main(int argc, char** argv) {
 
 	OptionManager om(argc, argv);
 
-	int my_var = 0;
-	om.option("update").required().var(&my_var).description("Update a variable.");
+	string str_var = "";
+	int int_var = 0;
+	float float_var = 0.0;
+	bool bool_var = false;
+
+	om.option("string").required().var(&str_var).description("Update a string variable.");
+	om.option("int").required().var(&int_var).description("Update a integer variable.");
+	om.option("float").required().var(&float_var).description("Update a float variable.");
+	om.option("bool").required().var(&bool_var).description("Update a boolean variable.");
 
 	try {
-		cout << "my_var=" << my_var << endl;
+		cout << "str_var  =" << str_var << endl;
+		cout << "int_var  =" << int_var << endl;
+		cout << "float_var=" << float_var << endl;
+		cout << "bool_var =" << bool_var << endl;
 		
 		om.process();
 
-		cout << "my_var=" << my_var << endl;
+		cout << "str_var  =" << str_var << endl;
+		cout << "int_var  =" << int_var << endl;
+		cout << "float_var=" << float_var << endl;
+		cout << "bool_var =" << bool_var << endl;
 		
 	} catch(clipp::error::InvalidOption& e) {
 		cerr << "ERROR: " << e.what() << endl;
